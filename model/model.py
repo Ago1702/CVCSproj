0e8e89eb06a00ddea1bd453f3bcfbb936a2f799a
Ago1702 <85801687+Ago1702@users.noreply.github.com>
2024-08-31 04:04:13 +0200

    Refactoring: Spostato il modulo MultiCBAM cambiato nome a cbam.py

--- model/model.py
+++ model/model.py
@@ -1,50 +1,21 @@
 import torch
 import math
 import torch.nn as nn
-from moduli.cbam import CBAM
+from moduli.modules import MultiCBAM, CBAM
 
-#
-# Idea di base
-# Usare 3 moduli di attenzione differenti
-# Questi hanno, per l'elemento, spaziale una differente dimensionalità
-# In questo modo si prova a concentrarsi su caratteristiche di dimensione differente
-# Questo perchè, a differenza di un vero blocco di attenzione,
-# CBAM usa delle CNN per capire dove concentrarsi.
-# Usando 3 dimensioni di kernel diverse si punta a dare una visione:
-# - Specifica   dim = (3, 3)
-# - Generale    dim = (15, 15)
-# - Intermedia  dim = (7, 7)
-# del fenomeno
-#
-# !!Questo elemento deve ancora essere valutato!!
-# Il risultato finale viene unito eseguendo una media
-# 
-# L'idea è, di base, quella della GoogleNet 2014
-#  
-
-class MultiCBAM(nn.Module):
-    def __init__(self, channel:int, r:float | int | list[int|float] = 16):
-        super(MultiCBAM, self).__init__()
-        if isinstance(r, list):
-            self.bam_small = CBAM(channel, 3, r[0])
-            self.bam_medium = CBAM(channel, 7, r[1])
-            self.bam_large = CBAM(channel, 15, r[2])
-        else:
-            self.bam_small = CBAM(channel, 3, r)
-            self.bam_medium = CBAM(channel, 7, r)
-            self.bam_large = CBAM(channel, 15, r)
-        self.conv1 = nn.Conv2d(9, 3, 1)
-    
-    def forward(self, x:torch.Tensor) -> torch.Tensor:
-        x_small = self.bam_small(x)
-        x_medium = self.bam_medium(x)
-        x_large = self.bam_large(x)
-        x_final = torch.concat([x_small, x_medium, x_large], 1)
-        x_final = self.conv1(x_final)
-        return x + x_final
-        
 if __name__ == "__main__":
-    net = nn.Sequential(MultiCBAM(3))
-    x = torch.rand([1, 3, 4, 4])
-    print(x)
-    print(net.forward(x))
+    net = nn.Sequential(
+        nn.Conv2d(3, 9, 3, padding=2),
+        nn.Conv2d(9, 27, 4, padding=3),
+        nn.Conv2d(27, 81, 5, padding=4),
+        MultiCBAM(81, 8),
+        MultiCBAM(81, 16),
+        MultiCBAM(81, 32),
+        nn.Conv2d(81, 27, 5),
+        nn.Conv2d(27, 9, 4),
+        nn.Conv2d(9, 3, 3),
+        nn.Conv2d(3, 1, 3)
+    )
+    x = torch.rand((2, 3, 120, 120))
+    x = net.forward(x)
+    print(x.shape)
