3ff1d75e3fd6e80de27c2bb4cf5fad25a4625f9b
homarone00 <269868@studenti.unimore.it>
2024-10-20 23:27:08 +0200

    added a custom dataloader

---
+++ model/dataloader.py
@@ -0,0 +1,91 @@
+import torch
+import os
+from PIL import Image
+from torch.utils.data import DataLoader
+from iter_dataset import DirectoryRandomDataset
+from utils.transform import RandomTransform
+from torchvision.utils import save_image
+
+
+class TransformDataLoader(DataLoader):
+    '''
+    A dataloader that transforms the images
+
+    This class takes a batch of images provided by a DirectoryRandomDataset, transforms them using a 
+    RandomTransform (either LOCAL or GLOBAL), then puts them in a 4 dimensional tensor of dimensions:
+    (batch_size, num_channels, height, width)
+    The dataloader automatically moves the files onto the cuda device, checking its presence during initialization.
+
+    Usage: call the constructor, then iterate over the object. Each time you iterate you get two tensors:
+    The first tensor is a batch of batch_size * images
+    The second tensor is the tensor of the labels
+
+    Methods:
+        custom_collate(batch) : a function declared as a method to be class aware. stacks images into a unique tensor
+
+    '''
+    def __init__(self, cropping_mode:int ,dataset:DirectoryRandomDataset, num_workers: int, batch_size:int =32,num_channels:int = 3):
+        #error handling
+        if not torch.cuda.is_available():
+            raise RuntimeError('CUDA not available (????????)')
+        if cropping_mode != RandomTransform.GLOBAL_CROP and cropping_mode != RandomTransform.LOCAL_CROP:
+            raise RuntimeError(f'TransformDataLoader was called with an invalid cropping_mode: {cropping_mode} .Look at the TransformDataLoader documentation.')
+        
+        super().__init__(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, collate_fn=self.custom_collate)
+        self.transform = RandomTransform(cropping_mode=cropping_mode) #using default constructor values 
+        self.cropping_mode = cropping_mode
+        self.num_channels = num_channels
+    
+
+    def custom_collate(self,batch):
+        '''
+        A custom version of the collate function.
+
+        This method is a custom version of the default one. Its role is taking a list of samples from the dataset, transforming and stacking them.
+
+        Args:
+            batch (list[tuple[torch.Tensor , torch.Tensor]]) : the batch, in which the first tensor is the image, and the second is the label
+
+        Returns:
+            tuple[torch.Tensor , torch.Tensor] : a tuple, containing the stacked images and the stacked labels
+        '''
+
+        
+        crop_size: tuple[int,int] = (0,0)
+        if self.cropping_mode == RandomTransform.LOCAL_CROP:
+            crop_size = RandomTransform.LOCAL_CROP_SIZE
+        elif self.cropping_mode == RandomTransform.GLOBAL_CROP:
+            crop_size = RandomTransform.GLOBAL_CROP_SIZE
+        else:
+            raise RuntimeError("TransformDataLoader found an invalid cropping_mode = ({cropping_mode[0]},{cropping_mode[1]}) during collate function execution.")
+
+        x_batch_list: list[torch.Tensor] = []
+        y_batch_list: list[torch.Tensor] = []
+        
+        for image, label in batch:
+            x_batch_list.append(self.transform(image))
+            y_batch_list.append(label)
+ 
+        x_batch_tensor: torch.Tensor = torch.stack(x_batch_list)
+        y_batch_tensor: torch.Tensor = torch.stack(y_batch_list)
+
+        return x_batch_tensor , y_batch_tensor
+
+'''
+if __name__ == "__main__":
+    dataset = DirectoryRandomDataset('/work/cvcs2024/VisionWise/train')
+    data_loader = TransformDataLoader(cropping_mode=RandomTransform.GLOBAL_CROP,dataset=dataset,num_workers=1)
+    for images, labels in data_loader:
+        for i in range(images.size(0)):
+            
+            path=''
+            if labels[i]==DirectoryRandomDataset.FAKE:
+                path = os.path.expanduser(f'~/CVCSproj/outputs/garbage/FAKE_{i}.PNG')
+            elif labels[i]==DirectoryRandomDataset.REAL:
+                path = os.path.expanduser(f'~/CVCSproj/outputs/garbage/REAL_{i}.PNG')
+            else:
+                raise RuntimeError(f'Unexpected label found: {labels[i]}')
+            
+            save_image(images[i],path)
+
+        break'''
