dada34f40009bcdc81771dfee6ea2d9f850202dd
homarone00 <269868@studenti.unimore.it>
2024-10-24 12:25:00 +0200

    fixed (hopefully) the dataset problems. modified the experimental model

--- model/model_experiment.py
+++ model/model_experiment.py
@@ -1,67 +1,101 @@
+import sys
+import os
+sys.path.append(os.path.expanduser('/homes/ocarpentiero/CVCSproj/model/'))
+
 import torch
 import torch.nn as nn
-import os
-import sys
+import torch.optim as optim
 from utils.modules import MultiCBAM
 from iter_dataset import DirectoryRandomDataset
 from utils.transform import RandomTransform
-import torch.optim as optim
 from dataloader import TransformDataLoader
+
 
 class DummyCBAM_1(nn.Module):
     def __init__(self):
         super(DummyCBAM_1, self).__init__()
         # defining layers
         self.net = nn.Sequential(
-        nn.Conv2d(3, 9, 3, padding=2),
-        nn.Conv2d(9, 27, 4, padding=3),
-        nn.Conv2d(27, 81, 5, padding=4),
-        MultiCBAM(81, 8),
-        MultiCBAM(81, 16),
-        MultiCBAM(81, 32),
-        nn.Conv2d(81, 27, 5),
-        nn.MaxPool2d(kernel_size=3,stride=2),
-        nn.Conv2d(27, 9, 4),
-        nn.MaxPool2d(kernel_size=3,stride=2),
-        nn.Conv2d(9, 3, 3),
-        nn.MaxPool2d(kernel_size=3,stride=2),
-        nn.Conv2d(3, 1, 3),
-        nn.MaxPool2d(kernel_size=3,stride=2),
+        nn.Conv2d(in_channels=3,out_channels=32,kernel_size=3,stride=1,padding=1), #3x200x200 --> 32x200x200
+        nn.MaxPool2d(kernel_size=2,stride=1), #32x200x200 --> 32x199x199
+        nn.Conv2d(in_channels=32,out_channels=64,kernel_size=3,stride=1,padding=1), #32x199x199 --> 64x199x199
+        nn.MaxPool2d(kernel_size=2,stride=1,padding=1), #64x199x199 --> 64x200x200
+        nn.Conv2d(in_channels=64,out_channels=128,kernel_size=3,stride=1,padding=1), #64x200x200 --> 128x200x200
+        nn.MaxPool2d(kernel_size=2,stride=1,padding=1), #128x200x200 --> 128x201x201
+        nn.Conv2d(in_channels=128,out_channels=64,kernel_size=3,stride=1,padding=1), #128x201x201 --> 64x201x201
+        nn.MaxPool2d(kernel_size=3,stride=2), #64x101x101
+        nn.Conv2d(in_channels=64,out_channels=16,kernel_size=3,stride=1,padding=1), #64x101x101 --> 16x101x101
+        nn.MaxPool2d(kernel_size=3,stride=2), #16x101x101 --> 16x51x51
+        nn.Conv2d(in_channels=16,out_channels=4,kernel_size=3,stride=1), #16x51x51 --> 4x49x49
+        nn.MaxPool2d(kernel_size=3,stride=2), #4x49x49 --> 4x25x25
+        nn.Conv2d(in_channels=4,out_channels=1,kernel_size=3,stride=1), #4x25x25 --> 1x23x23
+        nn.MaxPool2d(kernel_size=2,stride=1), #1x21x21 --> 1x20x20
         nn.Flatten(),
+        )
+        self.fully_connected = nn.Sequential(
+        nn.Linear(400,100),
+        nn.ReLU(),
         nn.Linear(100,1),
         nn.Sigmoid()
-    )
+        )
+    
         
     def forward(self,x:torch.Tensor):
-        return self.net.forward(x)
+        conv=self.net.forward(x)
+        return self.fully_connected.forward(conv)
     
 if __name__ == '__main__':
+    torch.backends.cudnn.enabled = False
+
     # checking if CUDA is available
     if not torch.cuda.is_available():
         raise RuntimeError("CUDA is not available. Did you use the slurm force?")
     dataset = DirectoryRandomDataset('/work/cvcs2024/VisionWise/test')
-    dataloader = TransformDataLoader(RandomTransform.GLOBAL_CROP,dataset,num_workers=1)
+    dataloader = TransformDataLoader(RandomTransform.GLOBAL_CROP,dataset,num_workers=1,batch_size=20,probability=0.4,pacman=True)
     model = DummyCBAM_1().cuda()
 
-    criterion = nn.BCELoss()  # Binary Cross Entropy Loss
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    #let's make it parallel
+    model = nn.DataParallel(model)
 
+    num_params = sum(p.numel() for p in model.parameters())
+    param = next(model.parameters())
+    param_size_in_bytes = num_params * param.element_size() 
+    print(param_size_in_bytes)
 
-    for images, labels in dataloader:
-        
-        #model in training moden
-        model.train()
+    #definition of the loss and the gradient descent thing
+    criterion = nn.BCELoss()  
+    optimizer = optim.Adam(model.parameters(),lr=0.001)
 
-        #zeroing the gradient in the optimizer
-        optimizer.zero_grad()
-        
+    #model in training moden
+    model.train(True)
+
+    #loading the weights
+    if os.path.exists(os.path.expanduser('~/CVCSproj/model/weights/DummyCBAM_1.pth')):
+        model.load_state_dict(torch.load(os.path.expanduser('~/CVCSproj/model/weights/DummyCBAM_1.pth')))
+        print('weights loaded')
+
+    optimizer.zero_grad()
+    
+    batch_size = 5000
+    
+    for index, (images, labels) in enumerate(dataloader):
+        if (index+1) % 20 ==0:
+            print(f'iteration number: {index + 1}',flush=True)
+            
+        '''print(f'max = {torch.max(images)}')
+        print(f'min = {torch.min(images)}')
+        print(f'avg = {torch.mean(images)}')'''
         #forward pass
         outputs = model(images)
         
-        #calcolo della loss
-        loss = criterion(outputs, labels)
+        #calcolo della loss (conversione a float32 per renderlo compatibile con la cross entropy)
+        loss = criterion(outputs, labels.to(torch.float32))
+        loss.backward() 
+
+        #ottimizzazione
+        if (index + 1)%batch_size == 0:
+            optimizer.step()
+            print(f'Batch {index + 1}: Loss: {loss.item():.4f}',flush=True)  # Print the loss
+            optimizer.zero_grad()
+            torch.save(model.state_dict(), os.path.expanduser('~/CVCSproj/model/weights/DummyCBAM_1.pth'))
         
-        #backprop e ottimizzazione
-        loss.backward()
-        optimizer.step()
-        print('Loss: {loss.item():.4f}')
--- slurm/script.sh
+++ slurm/script.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name=two_block    # Job name
-#SBATCH --output=output.txt         # Output file
-#SBATCH --error=error.txt           # Error file
-#SBATCH --time=24:00:00             # Time limit (HH:MM:SS)
-#SBATCH --partition=all_usr_prod      # Partition
-#SBATCH --gres=gpu:0                # Request one GPU
-#SBATCH --account=cvcs2024          # Account name
-#SBATCH --mem=10G                   # Request 10 GB of RAM
-#SBATCH --cpus-per-task=4
+#SBATCH --job-name=VisionWise                           # Job name
+#SBATCH --output=outputs/output.txt                     # Output file
+#SBATCH --error=outputs/error.txt                       # Error file
+#SBATCH --time=24:00:00                                 # Time limit (HH:MM:SS)
+#SBATCH --partition=all_usr_prod                  # all_serial all_usr_prod
+#SBATCH --gres=gpu:2                                    # Request one GPU
+#SBATCH --account=cvcs2024                              # Account name
+#SBATCH --mem=20G                                       # Request 20 GB of RAM
+#SBATCH --cpus-per-task=8
+
 # Run the Python script
 # istruzioni: 
 # - clonare la git nella home directory
 # - cambiare il file in "python ~/CVCSproj/percorso_al_file"
 
 
-cd ~/CVCSproj/dataset_generation
+cd ~/CVCSproj/slurm
 
+export PYTHONPATH=~/CVCSproj:$PYTHONPATH
 source ~/CVCSproj/.venv/bin/activate
-python3 renamer.py
+python3  ~/CVCSproj/model/model_experiment.py
